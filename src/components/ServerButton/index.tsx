68181a63e1fbadb6980032595a55c0dee3b8ba33
RAFAEL RODRIGO CORREIA DE LIMA <rafael.lima@gsw.com.br>
2020-09-14 09:13:47 -0300

    Components ServerButton and ServerName working

--- src/components/ServerButton/index.tsx
+++ src/components/ServerButton/index.tsx
@@ -1,7 +1,5 @@
 import React from 'react';
 
-import Logo from '../../assets/Logo.svg';
-
 import { Button } from './styles';
 
 export interface Props {
---
+++ src/stories/ServerButton.stories.tsx
@@ -0,0 +1,6 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+
+import { ServerButton } from '../components/ServerButton/'
+
+storiesOf('ServerButton', module).add('Default', () => < ServerButton/>)
