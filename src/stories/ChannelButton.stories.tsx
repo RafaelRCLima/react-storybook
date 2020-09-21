ac13a2467a88f922d6a258de1d4a247968c3677a
Rafael Lima <rafael.lima19-92@hotmail.com>
2020-09-21 17:19:31 -0300

    Base style altered

--- src/stories/ChannelButton.stories.tsx
+++ src/stories/ChannelButton.stories.tsx
@@ -1,17 +1,29 @@
 import React from 'react'
 
 import { ChannelButton } from '../components/ChannelButton'
+import { storiesOf } from '@storybook/react'
+// import { action } from '@storybook/addon-actions'
+import { withInfo } from '@storybook/addon-info'
 
 import GlobalStyles from '../styles/GlobalStyles'
 
-export default {
-  title: 'ChannelButton',
-  component: ChannelButton
-}
+storiesOf('Channel | Button', module)
+  .addDecorator(withInfo)
+  .add('Button', () => (
+    <>
+      <ChannelButton channelName='Channel' />
+      <GlobalStyles />
+    </>
+  ))
 
-export const ChannelButtonV1 = () => (
-  <>
-    <ChannelButton channelName="Channel"/>
-    <GlobalStyles />
-  </>
-)
+// export default {
+//   title: 'ChannelButton',
+//   component: ChannelButton
+// }
+
+// export const ChannelButtonV1 = () => (
+//   <>
+//     <ChannelButton channelName="Channel"/>
+//     <GlobalStyles />
+//   </>
+// )
